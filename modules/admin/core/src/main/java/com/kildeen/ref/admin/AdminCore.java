862c8526018c93e7f3a112f078c5f4a6b3d4f9a7
Karl Kildén <karl.kilden@gmail.com>
2014-04-13 01:17:25 +0200

    Initial place holder files. Added modularity concept.

---
+++ modules/admin/core/src/main/java/com/kildeen/ref/admin/AdminCore.java
@@ -0,0 +1,17 @@
+package com.kildeen.ref.admin;
+
+/**
+ * <p>File created: 2014-04-13 00:35</p>
+ *
+ * @version 1.0
+ * @author: Karl Kildén
+ * @since 1.0
+ */
+public class AdminCore {
+
+    private String helloWorld ="Hello, World!!!";
+
+    public String getHelloWorld() {
+        return helloWorld;
+    }
+}
